03e930ddcec2b8b39f568c2fa8554160821c54d6
vikgerasim <gerasimenkos@shaw.ca>
2023-12-09 18:13:53 -0700

    Error fixed

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,8 +1,8 @@
 //sets up the on-click function for the submit button to replace the page content with a paragraph in font size 24
     document.getElementById('submit-button').addEventListener('click', function() {
-      let newParagraph = document.createElement('p')
+      let newParagraph = document.createElement('p');
       newParagraph.innerHTML = 'Thank you for your message!';
       newParagraph.style.fontSize = '24px';
-      document.getElementById('contact-page').innerHTML = ''
+      document.getElementById('contact-page').innerHTML = '';
       document.getElementById('contact-page').appendChild(newParagraph);
     });
